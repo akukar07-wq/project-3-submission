c44509b960f6dd0e61c68811de1b1b9757badecf
akukar07-wq <akukar07@gmail.com>
2026-03-13 02:19:48 -0700

    Update NeuralNetwork.cpp

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -1,111 +1,159 @@
-#ifndef NEURAL_NET_HPP
-#define NEURAL_NET_HPP
+#include "NeuralNetwork.hpp"
+#include "Trace.hpp"
+#include <algorithm>
+using namespace std;
 
-// includes
-#include "Graph.hpp"
-#include "DataLoader.hpp"
+// NeuralNetwork -----------------------------------------------------------------------------------------------------------------------------------
 
-// class declarations
+void NeuralNetwork::eval() {
+    evaluating = true;
+}
 
-// The NeuralNetwork class inherits from the Graph class, because a NeuralNetwork is a more specific type of Graph!
-// You have not learned about inherited classes yet, but for now, treat the NeuralNetwork class as if it were also of type Graph.
-class NeuralNetwork : public Graph {
+void NeuralNetwork::train() {
+    evaluating = false;
+}
 
-    public:
+void NeuralNetwork::setLearningRate(double lr) {
+    learningRate = lr;
+}
 
-        // use these constructors for building your neural network manually through code
-        NeuralNetwork();
-        NeuralNetwork(int size);
+void NeuralNetwork::setInputNodeIds(std::vector<int> inputNodeIds) {
+    NeuralNetwork::inputNodeIds = inputNodeIds;
+}
 
-        // use these constructors for loading a premade architecture from a file
-        NeuralNetwork(std::string filename);
-        NeuralNetwork(std::istream& in);
-        // gets the layer structure (each inner vector is the node ids in that layer).
-        const std::vector<std::vector<int> >& getLayers() const;
+void NeuralNetwork::setOutputNodeIds(std::vector<int> outputNodeIds) {
+    NeuralNetwork::outputNodeIds = outputNodeIds;
+}
+
+vector<int> NeuralNetwork::getInputNodeIds() const {
+    return vector<int>(inputNodeIds);
+}
+
+vector<int> NeuralNetwork::getOutputNodeIds() const {
+    return vector<int>(outputNodeIds);
+}
+
+vector<double> NeuralNetwork::predict(DataInstance instance) {
+    flush();
+    vector<double> input = instance.x;
+
+    if (input.size() != inputNodeIds.size()) {
+        cerr << "input size mismatch." << endl;
+        cerr << "\tNeuralNet expected input size: " << inputNodeIds.size() << endl;
+        cerr << "\tBut got: " << input.size() << endl;
+        return vector<double>();
+    }
+
+    for (int i = 0; i < inputNodeIds.size(); i++) {
+        int nodeId = inputNodeIds[i];
+        nodes[nodeId]->preActivationValue = input[i];
+        nodes[nodeId]->postActivationValue = input[i];
+    }
+
+    vector<int> inDegree(size, 0);
+    for (int i = 0; i < size; i++) {
+        for (auto& pair : adjacencyList[i]) {
+            inDegree[pair.first]++;
+        }
+    }
+
+    vector<int> processedPreds(size, 0);
+
+    queue<int> q;
+    vector<bool> visited(size, false);
+    for (int i = 0; i < inputNodeIds.size(); i++) {
+        q.push(inputNodeIds[i]);
+        visited[inputNodeIds[i]] = true;
+    }
+
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+
+        bool isInput = find(inputNodeIds.begin(), inputNodeIds.end(), current) != inputNodeIds.end();
+        if (!isInput) {
+            visitPredictNode(current);
+        }
+
+        for (auto& pair : adjacencyList[current]) {
+            int neighborId = pair.first;
+            Connection& conn = pair.second;
+            visitPredictNeighbor(conn);
+            processedPreds[neighborId]++;
+            if (processedPreds[neighborId] == inDegree[neighborId] && !visited[neighborId]) {
+                visited[neighborId] = true;
+                q.push(neighborId);
+            }
+        }
+    }
+
+    vector<double> output;
+    for (int i = 0; i < outputNodeIds.size(); i++) {
+        int dest = outputNodeIds.at(i);
+        output.push_back(nodes.at(dest)->postActivationValue);
+    }
+
+    if (evaluating) {
+        flush();
+    } else {
+        batchSize++;
+        contribute(instance.y, output.at(0));
+    }
+    return output;
+}
+
+bool NeuralNetwork::contribute(double y, double p) {
+    contributions.clear();
+    for (auto node : inputNodeIds) {
+        contribute(node, y, p);
+    }
+    return true;
+}
+
+double NeuralNetwork::contribute(int nodeId, const double& y, const double& p) {
+    visitContributeStart(nodeId);
+    double incomingContribution = 0;
+    double outgoingContribution = 0;
+
+    if (contributions.count(nodeId) > 0) {
+        return contributions[nodeId];
+    }
+
+    if (adjacencyList.at(nodeId).empty()) {
+        outgoingContribution = -1 * ((y - p) / (p * (1 - p)));
+    } else {
+        for (auto& pair : adjacencyList[nodeId]) {
+            Connection& conn = pair.second;
+            incomingContribution = contribute(pair.first, y, p);
+            visitContributeNeighbor(conn, incomingContribution, outgoingContribution);
+        }
+        bool isInput = find(inputNodeIds.begin(), inputNodeIds.end(), nodeId) != inputNodeIds.end();
+        if (!isInput) {
+            visitContributeNode(nodeId, outgoingContribution);
+        }
+    }
+
+    contributions[nodeId] = outgoingContribution;
+    return outgoingContribution;
+}
+
+bool NeuralNetwork::update() {
+    for (int i = 0; i < size; i++) {
+        NodeInfo* node = nodes[i];
+        node->bias = node->bias - (learningRate * node->delta);
+        node->delta = 0;
+        for (auto& pair : adjacencyList[i]) {
+            Connection& conn = pair.second;
+            conn.weight = conn.weight - (learningRate * conn.delta);
+            conn.delta = 0;
+        }
+    }
+    flush();
+    return true;
+}
 
 
-        // TODO: getters / setters
-        // Put the neural network in "eval" mode. In "eval" mode, the neural network will not accumulate gradients.
-        void eval();
-        // Put the neural network in "train" mode. In "train" mode, the neural network will accumulated gradients.
-        void train();
-        // sets the learning rate of the neural network: how impactful each gradient contribution is at each step.
-        void setLearningRate(double lr);
-        // contains set of node ids which correspond to input nodes
-        void setInputNodeIds(std::vector<int> inputNodeIds);
-        // contains a set of node ids which correspond to output nodes
-        void setOutputNodeIds(std::vector<int> outputNodeIds);
-        // gets the id's of every input node. 
-        std::vector<int> getInputNodeIds() const;
-        // gets the id's of every output node.
-        std::vector<int> getOutputNodeIds() const;
-
-
-        // TODO: Neural Network Implementations
-        // Flow data instance into the neural network and compute the predicted value. 
-        std::vector<double> predict(DataInstance instance);
-        // Apply accumumated gradients to every weight and every bias
-        bool update();
 
 
 
-        // Other useful methods...
-        // Calculates the neural networks accuracy given a dataset in the form of a DataLoader object.
-        double assess(DataLoader dl);
-        // Calculates the nerual networks accuracy given a dataset stored in the given filename. 
-        double assess(std::string filename);
-        // Saves the model structure, weights, and biases into the given filename. 
-        void saveModel(std::string filename);
-        friend std::ostream& operator<<(std::ostream& out, const NeuralNetwork& nn);
 
-    private:
-
-        // TODO: given the neural nets prediction and the ground truth label, it accumulates gradients for weights and biases.
-        // each contribution is accumulated in a variable called delta, which resides in every node (for the bias) and every connection (for the weight)
-        // This is the entry point: it initiates the recursion from the input layer.
-        bool contribute(double y, double p);
-        // This is the recursive helper for contribute(double y, double p).
-        // It performs the DFT, visiting one node per call and recursing into its neighbors.
-        double contribute(int nodeId, const double& y, const double& p);
-
-         // VERY helpful visit methods for predict and contribute
-        // visits the node during evaluation
-        void visitPredictNode(int vId);
-        // visits v's neighbor during evaluation
-        void visitPredictNeighbor(Connection c);
-        // visits the start of the contribution step
-        void visitContributeStart(int vId);
-        // visits the node during contribution
-        void visitContributeNode(int vId, double& outgoingContribution);
-        // visits the connection during contribution
-        void visitContributeNeighbor(Connection& c, double& incomingContribution, double& outgoingContribution);
-
-
-        // other helpful and members...
-        // indicates the number of training instances seen before the neural network updates
-        double batchSize;
-        // eval or train mode. evaluating = true indicates eval mode. 
-        bool evaluating;
-        // controls the impact of each gradient contribution. 
-        double learningRate;
-        // each index of layers holds a vector which contains the id's of every node in that layer. 
-        std::vector<std::vector<int> > layers;
-        // keeps track of which contributions have already been made.
-        std::unordered_map<int, double> contributions;
-        // contains ids of input nodes
-        std::vector<int> inputNodeIds;
-        // contains ids of output nodes
-        std::vector<int> outputNodeIds;
-
-
-        // other helpful methods...
-        // loads neural network structure from the input file stream
-        void loadNetwork(std::istream& in);
-        // refreshes node values for the next computation.
-        void flush();
-
-        // since NeuralNetwork inherits from Graph, you can imagine all of the graph members here as well...
-
-};
-
-#endif
